d76dbaa2a5ba94d64f68d08eab881094d9ca7925
emeka <emekaowo@yahoo.com>
2025-08-06 11:12:50 +0100

    fix menu icon

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -91,13 +91,13 @@ const Navbar = () => {
         {/* Mobile menu button - Updated icon color */}
         <button
           className={`md:hidden ${
-            isScrolled ? "text-indigo-900" : "text-white"
+            isScrolled ? "text-indigo-900" : "text-[#ffcc29]"
           }`}
           onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
+            className="h-7 w-7"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
